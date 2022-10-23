07d1aba080edda809c416b837bbe90762bd9b15a
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2022-10-23 22:05:18 +0200

    update package

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const connect = () => {
 }
 
 app.use(cors());
-app.use(cookieParser());
+app.use(cookieParser({ secure: true, samesite: "none" }));
 app.use(express.json());
 app.use("/api/auth", authRouters);
 app.use("/api/user", userRouters);
