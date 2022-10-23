f9e3afe366ca579b9a0a50261a63b2b2845a4482
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2022-10-23 21:58:00 +0200

    update package

--- verifyToken.js
+++ verifyToken.js
@@ -3,6 +3,7 @@ import { createError } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
+    console.log(token)
     if (!token) return next(createError(401, "You are not authenticated"));
 
     jwt.verify(token, process.env.JWT, (err, user) => {
