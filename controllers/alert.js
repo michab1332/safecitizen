390646e70d30022ec29f5f58230a4c0a46d9b85b
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2022-10-30 14:15:03 +0100

    fix auth functions

--- controllers/alert.js
+++ controllers/alert.js
@@ -3,10 +3,10 @@ import Alert from "../models/Alert.js";
 import User from "../models/User.js";
 
 export const addAlert = async (req, res, next) => {
-    const newAlert = new Alert({ ...req.body });
+    const newAlert = new Alert({ ...req.body, userId: req.user.id });
     try {
         await newAlert.save();
-        await User.findByIdAndUpdate(req.body.userId, {
+        await User.findByIdAndUpdate(req.user.id, {
             $push: { alerts: newAlert._id }
         })
         res.status(200).json("Alert has been created");
